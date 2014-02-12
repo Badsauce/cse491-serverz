dfe1e8cdfcf5862720f87d86cb2247b011a00eb1
Nicholas Rutowski <nickrutowski@gmail.com>
2014-02-11 19:36:14 -0500

    made basic pages work with wsgi

--- app.py
+++ app.py
@@ -1,147 +1,64 @@
-#!/usr/bin/env python
-import random
-import socket
-import time
-import urlparse
 import jinja2
+import re
 
 from cgi import parse_qs, escape
 
-from StringIO import StringIO
-
-def base_app(environ, start_repsonse):
-    parameters = parse_qs(environ.get('QUERY_STRING', ''))
-    if 'subject' in parameters:
-        subject = escape(parameters['subject'][0])
-    else:
-        subject = 'World'
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello %(subject)s" % {'subject': subject}]
+def base_app(environ, start_response):
+    return handle_connection(environ,start_response)
 
 def make_app():
     return base_app
 
-def handle_submit(conn,url,env):
+def handle_submit(environ, start_response, jinja):
     query = urlparse.parse_qs(url.query)
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n\r\n')
+    start_response('200 OK', [('Content-Type', 'text/html')])
     vars = dict(firstname = query['firstname'][0],lastname = query['lastname'][0])
     conn.send(env.get_template("submit.html").render(vars))
-def handle_form(conn,url,env):
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n\r\n')
+
+def handle_form(environ, start_response, jinja):
+    start_response('200 OK', [('Content-Type', 'text/html')])
     conn.send(env.get_template('form.html').render())
 
-def handle_root(conn, url,env):
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n\r\n')
-    conn.send(env.get_template('index.html').render())
+def handle_root(environ, start_response, jinja):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return jinja.get_template('index.html').render()
 
-def handle_content(conn, url,env):
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n\r\n')
-    conn.send(env.get_template('content.html').render())
+def handle_content(environ, start_response, jinja):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return jinja.get_template('content.html').render()
 
-def handle_file(conn, url,env):
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n\r\n')
-    conn.send(env.get_template('file.html').render())
-def handle_image(conn, url,env):
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n\r\n')
-    conn.send(env.get_template('image.html').render())
-def handle_404(conn, url, env):
-    conn.send('HTTP/1.0 404 Not Found\r\n')
-    conn.send('Content-type: text/html\r\n\r\n')
-    conn.send(env.get_template("404.html").render())
+def handle_file(environ, start_response, jinja):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return jinja.get_template('file.html').render()
 
-def handle_get(conn, url, env):
-    path = url.path
-    if path == '/':
-        handle_root(conn,url,env)
-    elif path == '/form':
-        handle_form(conn,url,env)
-    elif path == '/submit':
-        handle_submit(conn,url,env)
-    elif path == '/content':
-        handle_content(conn, url,env)
-    elif path == '/file':
-        handle_file(conn, url,env)
-    elif path == '/image':
-        handle_image(conn, url,env)
-    else:
-        handle_404(conn,url,env)
+def handle_image(environ, start_response, jinja):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return jinja.get_template('image.html').render()
 
-def handle_post(conn,content,env):
+def handle_404(environ, start_response, jinja):
+    start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
+    return jinja.get_template("404.html").render()
+
+def handle_post(environ, start_response, jinja):
     conn.send('HTTP/1.0 200 OK\r\n')
     conn.send('Content-type: text/html\r\n\r\n')
     conn.send(env.get_template("submit.html").render(content))
-def read_head(conn):
-    message = ''
-    while '\r\n\r\n' not in message:
-        message += conn.recv(1)
-    return message.rstrip()
 
-def handle_connection(conn):
+def handle_connection(environ,start_response):
     loader = jinja2.FileSystemLoader('./templates')
-    env = jinja2.Environment(loader=loader)
+    jinja = jinja2.Environment(loader=loader)
 
-
-    rawHead = read_head(conn)
-    headList = rawHead.split('\r\n')
-    contentType = [s for s in headList if 'Content-Type' in s]
-    req = headList[0].split(' ')
-    reqType = req[0]
-    if reqType == 'GET':
-        path = req[1]
-        url = urlparse.urlparse(path)
-        handle_get(conn, url, env)
-    elif reqType == 'POST':
-        requestLine, raw_headers = rawHead.split('\r\n',1)
-        headers = raw_headers.split('\r\n')
-
-        headDict = {}
-        for line in headers:
-            k, v = line.split(': ', 1)
-            headDict[k.lower()] = v
-
-        content = conn.recv(int(headDict['content-length']))
-        contentType = headDict['content-type']
-
-        if contentType == 'application/x-www-form-urlencoded':
-            content = urlparse.parse_qs(content)
-            content['firstname'] = content['firstname'][0]
-            content['lastname'] = content['lastname'][0]
-        elif 'multipart/form-data' in contentType:
-            environ = {}
-            environ['REQUEST_METHOD'] = 'POST'
-
-            form = cgi.FieldStorage(headers=headDict, fp=StringIO(content), environ=environ)
-
-            content = {}
-            content['firstname'] = form['firstname'].value
-            content['lastname'] = form['lastname'].value
-        handle_post(conn,content,env)
-    conn.close()
-
-def main():
-    s = socket.socket()         # Create a socket object
-    host = socket.getfqdn() # Get local machine name
-    port = random.randint(8000, 9999)
-    s.bind((host, port))        # Bind to the port
-
-    print 'Starting server on', host, port
-    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
-
-    s.listen(5)                 # Now wait for client connection.
-
-    print 'Entering infinite loop; hit CTRL-C to exit'
-    while True:
-        # Establish connection with client.    
-        c, (client_host, client_port) = s.accept()
-        print 'Got connection from', client_host, client_port
-        handle_connection(c)
-
-if __name__ == "__main__":
-    main()
-
+    path = environ.get('PATH_INFO', '')
+    if path == '/':
+        content = handle_root(environ, start_response, jinja)
+    elif path == '/content':
+        content = handle_content(environ, start_response, jinja)
+    elif path == '/image':
+        content = handle_image(environ, start_response, jinja)
+    elif path == '/file':
+        content = handle_file(environ, start_response, jinja)
+    else:
+        content = handle_404(environ, start_response, jinja)
+    # flatten content form unicode to a string
+    content = content.encode('latin-1', 'replace')
+    return content
