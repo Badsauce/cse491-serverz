ea02df0d000ec1568a1fecb67166988c623fbb52
Nicholas Rutowski <nickrutowski@gmail.com>
2014-02-11 21:56:52 -0500

    added support for forms get/post/multipart

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import jinja2
 import re
 
-from cgi import parse_qs, escape
+from cgi import parse_qs, escape, FieldStorage
 
 def base_app(environ, start_response):
     return handle_connection(environ,start_response)
@@ -9,15 +9,32 @@ def base_app(environ, start_response):
 def make_app():
     return base_app
 
+def handle_post(environ, start_response, jinja):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n\r\n')
+    conn.send(env.get_template("submit.html").render(content))
 def handle_submit(environ, start_response, jinja):
-    query = urlparse.parse_qs(url.query)
     start_response('200 OK', [('Content-Type', 'text/html')])
-    vars = dict(firstname = query['firstname'][0],lastname = query['lastname'][0])
-    conn.send(env.get_template("submit.html").render(vars))
+    #query = urlparse.parse_qs(url.query)
+    path = environ.get('PATH_INFO', '')
+    method = environ.get('REQUEST_METHOD','')
+    content_type = environ.get('CONTENT_TYPE', '')
+    if "multipart" in content_type or content_type == "application/x-www-form-urlencoded":
+        stream = environ.get('wsgi.input','')
+        form = FieldStorage(fp=stream, environ=environ)
+        params = {}
+        params['firstname'] = form['firstname'].value
+        params['lastname'] = form['lastname'].value
+    elif method == "GET":
+        query = parse_qs(environ.get('QUERY_STRING',''))
+        params = {}
+        params['firstname'] = query['firstname'][0]
+        params['lastname'] = query['lastname'][0]
+    return jinja.get_template("submit.html").render(params)
 
 def handle_form(environ, start_response, jinja):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    conn.send(env.get_template('form.html').render())
+    return jinja.get_template('form.html').render()
 
 def handle_root(environ, start_response, jinja):
     start_response('200 OK', [('Content-Type', 'text/html')])
@@ -39,11 +56,6 @@ def handle_404(environ, start_response, jinja):
     start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
     return jinja.get_template("404.html").render()
 
-def handle_post(environ, start_response, jinja):
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n\r\n')
-    conn.send(env.get_template("submit.html").render(content))
-
 def handle_connection(environ,start_response):
     loader = jinja2.FileSystemLoader('./templates')
     jinja = jinja2.Environment(loader=loader)
@@ -57,6 +69,10 @@ def handle_connection(environ,start_response):
         content = handle_image(environ, start_response, jinja)
     elif path == '/file':
         content = handle_file(environ, start_response, jinja)
+    elif path == '/form':
+        content = handle_form(environ, start_response, jinja)
+    elif "/submit" in path:
+        content = handle_submit(environ, start_response, jinja)
     else:
         content = handle_404(environ, start_response, jinja)
     # flatten content form unicode to a string
